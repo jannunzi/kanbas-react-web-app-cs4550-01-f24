517751c47ee4241e3f63420c193e6618373a911e
Jose Annunziato <jannunzi@gmail.com>
2024-10-04 14:51:59 -0400

    kanbas a2

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,27 +4,22 @@ import Signin from "./Account/Signin";
 import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import "./styles.css";
 
 export default function Kanbas() {
   return (
-    <div>
-      <table>
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="Account" />} />
-              <Route path="/Account/*" element={<Account />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-              <Route path="/Courses/:cid/*" element={<Courses />} />
-              <Route path="/Calendar" element={<h2>Calendar</h2>} />
-              <Route path="/Inbox" element={<h2>Inbox</h2>} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+    <div id="wd-kanbas">
+      <KanbasNavigation />
+      <div className="wd-main-content-offset p-3">
+        <Routes>
+          <Route path="/" element={<Navigate to="Account" />} />
+          <Route path="/Account/*" element={<Account />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Courses/:cid/*" element={<Courses />} />
+          <Route path="/Calendar" element={<h2>Calendar</h2>} />
+          <Route path="/Inbox" element={<h2>Inbox</h2>} />
+        </Routes>
+      </div>
     </div>
   );
 }
