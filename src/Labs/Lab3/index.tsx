dc6cc09b7d07e4f27db59cbe51f6c2792bbf3b5c
Jose Annunziato <jannunzi@gmail.com>
2024-10-09 08:16:34 -0400

    lab3

---
+++ src/Labs/Lab3/index.tsx
@@ -0,0 +1,18 @@
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import IfElse from "./IfElse";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
+export default function Lab3() {
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <ConditionalOutputIfElse />
+    </div>
+  );
+}
